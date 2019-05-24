c57bcf17ea4a093cef3173035ec45f792583a2fb
Cristian Funck <cefunck@miuandes.cl>
2019-05-23 22:36:17 -0400

    Added privileges to purchase for logged user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
 
   def initialize(user)
     can :read, Event
+    if user.present?
+      can :purchase, Ticket
+    end
     # usuarios no registrados deben poder ver los eventos disponibles
     # usuarios registrados deben poder navegar por eventos, añadir tickets al carro de compras y pagar
     # usuarios administradores deben poder agregar eventos y tipos de ticket
